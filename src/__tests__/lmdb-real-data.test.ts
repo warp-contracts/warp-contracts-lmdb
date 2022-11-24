5bdc0b0100c233cfac0499c1538ee9309a5de714
Jan Olszak <jan@redstone.finance>
2022-11-24 11:30:17 +0100

    feat: test purge with real cache data, normally skipped

---
+++ src/__tests__/lmdb-real-data.test.ts
@@ -0,0 +1,10 @@
+import { LmdbCache } from '../LmdbCache';
+import { defaultCacheOptions, PruneStats } from 'warp-contracts';
+
+describe.skip('Prune real Lmdb cache. Copy the real cache to ./cache, unskip and run npm run test:real', () => {
+    test('handle real data', async () => {
+        const sut = new LmdbCache<any>({ ...defaultCacheOptions, inMemory: true });
+        const stats = await sut.prune(1)
+        console.log("Stats:", stats)
+    });
+});
