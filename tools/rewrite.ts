ffaf0fe1ac7a497864157eadae661c91ace20d5c
Jan Olszak <jan@redstone.finance>
2022-12-08 15:21:50 +0100

    feat: script for rewriting cache file #8
    Example:
    ts-node ./tools/rewrite.ts  -i ../src -o ../out

---
+++ tools/rewrite.ts
@@ -0,0 +1,59 @@
+/* eslint-disable */
+import { LmdbCache } from '../src/LmdbCache'
+import { RootDatabase } from 'lmdb';
+import { defaultCacheOptions, CacheKey } from 'warp-contracts';
+
+const commandLineArgs = require('command-line-args')
+const cliProgress = require('cli-progress');
+
+process.on('uncaughtException', (error: any) => {
+  console.error(`Uncaught exception: ${error}`);
+  if (error.stack)
+    console.error(error.stack);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (error: any) => {
+  console.error(`Promise rejection: ${error}`);
+  if (error.stack)
+    console.error(error.stack);
+  process.exit(1);
+
+});
+
+const optionDefinitions = [
+  { name: 'input', type: String, alias: 'i' },
+  { name: 'output', type: String, alias: 'o' },
+]
+
+async function main() {
+  const args = commandLineArgs(optionDefinitions)
+  if (args.input === args.output) {
+    console.error("Input and output folders can't be the same")
+    return
+  }
+
+  console.log('Rewriting LMDB cache to another LMDB cache');
+
+  const input = new LmdbCache<any>({ ...defaultCacheOptions, dbLocation: args.input })
+    .storage<RootDatabase<any, string>>();
+  const output = new LmdbCache<any>({ ...defaultCacheOptions, dbLocation: args.output })
+    .storage<RootDatabase<any, string>>();
+
+  const bar = new cliProgress.SingleBar({
+    etaBuffer: 1000,
+  }, cliProgress.Presets.shades_classic);
+  bar.start(input.getKeysCount(), 0);
+
+  input.transactionSync(() => {
+    input.getRange({ snapshot: false })
+      .forEach(({ key, value }) => {
+        output.putSync(key, value)
+        bar.increment()
+      });
+  })
+
+  bar.stop()
+}
+
+main().catch((e) => console.error(e));
