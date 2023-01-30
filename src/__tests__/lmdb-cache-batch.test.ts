097fe29bd7716a71b8ec6581d8f2b10b6cff22a9
Tadeuchi <tadeuszwl@gmail.com>
2023-01-30 11:59:23 +0100

    fix: childTransaction for batch

--- src/__tests__/lmdb-cache-batch.test.ts
+++ src/__tests__/lmdb-cache-batch.test.ts
@@ -16,20 +16,25 @@ describe('Lmdb cache batch', () => {
       delBatch('contract12'),
       putBatch('contract12', getSortKey(111), 'Result12'),
       putBatch('contract13', getSortKey(111), 'Result13'),
+      putBatch('contract13', getSortKey(112), 'Result13'),
       putBatch('contract11', getSortKey(111), 'Result1111'),
+      putBatch('contract11', getSortKey(112), 'Result1112'),
       delBatch('contract13'),
       delBatch(getContractId(0))
     );
 
     expect(await sut.get(new CacheKey(getContractId(0), getSortKey(0)))).toBeTruthy();
     expect(await sut.get(new CacheKey('contract11', getSortKey(111)))).toBeFalsy();
+    expect(await sut.get(new CacheKey('contract11', getSortKey(112)))).toBeFalsy();
     expect(await sut.get(new CacheKey('contract12', getSortKey(111)))).toBeFalsy();
 
     await sut.batch(batches);
 
     expect(await sut.get(new CacheKey(getContractId(0), getSortKey(0)))).toBeFalsy();
     expect((await sut.get(new CacheKey('contract11', getSortKey(111)))).cachedValue).toMatch('Result1111');
+    expect((await sut.get(new CacheKey('contract11', getSortKey(112)))).cachedValue).toMatch('Result1112');
     expect((await sut.get(new CacheKey('contract12', getSortKey(111)))).cachedValue).toMatch('Result12');
     expect(await sut.get(new CacheKey('contract13', getSortKey(111)))).toBeFalsy();
+    expect(await sut.get(new CacheKey('contract13', getSortKey(112)))).toBeFalsy();
   });
 });
